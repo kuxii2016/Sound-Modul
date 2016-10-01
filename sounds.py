538fa352953ca1fd1df66218ce677c78c45664a0
Michael K <kux.michael2@googlemail.com>
2016-10-01 03:46:37 +0200

    NOT WAV
    WAV zu wav geändert da die sounds sonst nicht abspielbar sind

--- sounds.py
+++ sounds.py
@@ -6,18 +6,18 @@ from signal import pause
 pygame.mixer.init(44100, 16, 2, 4096)
 
 sound_pins = {
-    2: Sound("Dampffiff7.WAV"),
-    3: Sound("E-Lok-Pfiff6.WAV"),
+    2: Sound("Dampffiff7.wav"),
+    3: Sound("E-Lok-Pfiff6.wav"),
     4: Sound("ICEHorn1.wav"),
 
-    27: Sound("Zugdurchfahrt.Gl.1.WAV"),
-    22: Sound("Zugdurchfahrt.Gl.2.WAV"),
-    25: Sound("Zugdurchfahrt.Gl.3.WAV"),
-     9: Sound("Zugdurchfahrt.Gl.4.WAV"),
-     6: Sound("Ans.zp9.Gl1.eig.WAV"),
-    13: Sound("Ans.zp9.Gl2.eig.WAV"),
-    19: Sound("Ans.zp9.Gl3.eig.WAV"),
-    26: Sound("Ans.zp9.Gl4.eig.WAV"),
+    27: Sound("Zugdurchfahrt.Gl.1.wav"),
+    22: Sound("Zugdurchfahrt.Gl.2.wav"),
+    25: Sound("Zugdurchfahrt.Gl.3.wav"),
+     9: Sound("Zugdurchfahrt.Gl.4.wav"),
+     6: Sound("Ans.zp9.Gl1.eig.wav"),
+    13: Sound("Ans.zp9.Gl2.eig.wav"),
+    19: Sound("Ans.zp9.Gl3.eig.wav"),
+    26: Sound("Ans.zp9.Gl4.eig.wav"),
 
 }
 
